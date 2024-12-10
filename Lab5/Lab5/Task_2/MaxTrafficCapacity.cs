721277853bfdf61d6ed983184caa4683c3b737c5
jasperin <jasperinina@mail.ru>
2024-12-10 23:12:05 +0500

    1

--- Lab5/Lab5/Task_2/MaxTrafficCapacity.cs
+++ Lab5/Lab5/Task_2/MaxTrafficCapacity.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Controls;
-using Lab5.Models;
+﻿using Lab5.Models;
 
 namespace Lab5.Task_2
 {
